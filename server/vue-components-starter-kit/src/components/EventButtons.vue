c3eba5512c69f537293105cb71e09278e1bbd97b
Kobatai <taiga1983346@gmail.com>
2019-12-09 22:28:05 +0900

    emitの追加

---
+++ server/vue-components-starter-kit/src/components/EventButtons.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="emitEventOne">Event 1</button>
+    <button @click="emitEventTwo">Event 2</button>
+    <button @click="emitEventThree">Event 3</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    emitEventOne () {
+      // 1つ目の引数はイベントの名前
+      this.$emit('event-one')
+    },
+    emitEventTwo () {
+      // ２つ目の引数はイベントハンドラに渡される引数
+      this.$emit('event-two', 'This is an argument')
+    },
+    emitEventThree () {
+      // ２つ目以降もイベントハンドラに渡される引数
+      this.$emit('event-three', 123, { name:'three'})
+    }
+  }
+}
+</script>
+
--- server/vue-components-starter-kit/src/index.js
+++ server/vue-components-starter-kit/src/index.js
@@ -1,11 +1,39 @@
 import Vue from 'vue'
 
 // Components
-import AnchorLink from './components/AnchorLink.vue'
+// import AnchorLink from './components/AnchorLink.vue'
 
-new Vue({
+// new Vue({
+//   el: '#app',
+//   components: {
+//     AnchorLink
+//   }
+// })
+
+
+// Components
+import EventButtons from './components/EventButtons.vue'
+
+const app = new Vue({
   el: '#app',
+  data: {
+    arguments: []
+  },
   components: {
-    AnchorLink
+    EventButtons
+  },
+  methods: {
+    onEventOne () {
+      alert('Event 1')
+    },
+    onEventTwo (argument) {
+      this.arguments = []
+      this.arguments.push(argument)
+    },
+    onEventThree (arg1, arg2) {
+      this.arguments = []
+      this.arguments.push(arg1)
+      this.arguments.push(arg2)
+    }
   }
 })
