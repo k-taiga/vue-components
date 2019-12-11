02757ddb34f71889f6da27aa696b33df65f8ab1e
Kobatai <taiga1983346@gmail.com>
2019-12-11 14:29:06 +0900

    scssの設定追加

--- server/vue-components-starter-kit/webpack.config.js
+++ server/vue-components-starter-kit/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: "none",
@@ -11,11 +12,20 @@ module.exports = {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.vue$/, loader: 'vue-loader' },
-      { test: /\.css$/, loader: ['vue-style-loader', 'css-loader'] }
+      { test: /\.css$/, loader: ['vue-style-loader', 'css-loader'] },
+      { test: /\.scss$/, use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      }
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'app.css'
+    })
   ],
   resolve: {
     alias: {
